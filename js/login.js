c84fb92356bff3ee084d49fa6813287a3f5415c8
GillbertDH <undohyng@gmail.com>
2026-05-27 18:37:09 +0900

    로그인이랑 메인 연결

--- js/login.js
+++ js/login.js
@@ -1,1 +1,10 @@
-fsdfgi
+//지금은 일단 그냥 로그인버튼 누르면 바로 main으로 넘어가도록 했는데
+// 나중에 로그인 검사하는거 추가해야할듯
+
+
+const loginBtn = document.querySelector('.login-button');
+
+loginBtn.addEventListener('click', function() {
+    
+    window.location.href = 'pages/main.html';
+});
